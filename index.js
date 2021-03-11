ce0cc197749bd70fa11109537bd867a3a37ffba3
Shota Iuchi <shotaiuchi.develop@gmail.com>
2021-03-11 23:02:46 +0900

    require

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const request = require('request');
 
 const path = require('path')
+
+const TEST_DOMAIN = 'https://www.google.com/?hl=ja';
 
 app.listen(3000, () => {
     console.log('Running at Port 3000...');
@@ -13,7 +16,7 @@ console.log(__dirname);
 app.use(express.json());
 
 //app.use(express.static(path.join(__dirname, 'public')))
-//app.use(express.static('public'))
+app.use(express.static('public'))
 // app.get('/', (req, res, next) => {
 //     res.send('ルートだよ！');
 // });
@@ -29,6 +32,36 @@ app.use(express.json());
 // app.get(/a/, function (req, res) {
 //     res.send('/a/')
 // })
+
+app.get('/th', (req, res) => {
+
+    request(TEST_DOMAIN + req.originalUrl)
+        // 成功時
+        .on('response', (response) => {
+            console.log(response.statusCode);
+            console.log(response.headers['content-type']);
+        })
+        // 失敗時
+        .on('error', function (err) {
+            console.log(err);
+            res.sendStatus(404);
+        })
+        .pipe(res);
+});
+
+app.get('/nuko/:width/:height', (req, res) => {
+    // 送受信の設定
+    const options = {
+        url: 'http://placekitten.com/' + `${req.params.width}/${req.params.height}`,
+        method: 'GET',
+        encoding: null
+    };
+
+    request(options, (err, response, body) => {
+        res.set('Content-Type', response.headers['content-type']);
+        res.send(body);
+    });
+});
 
 app.get('/ab*cd/', (req, res, next) => {
     res.send(req.path)
