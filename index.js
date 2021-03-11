8f308c01510256bf9e258e2067c996c3dc9d70e3
Shota Iuchi <shotaiuchi.develop@gmail.com>
2021-03-11 21:24:59 +0900

    api

--- index.js
+++ index.js
@@ -10,8 +10,33 @@ app.listen(3000, () => {
 console.log(__filename);
 console.log(__dirname);
 
+app.use(express.json());
+
 //app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.static('public'))
+//app.use(express.static('public'))
+// app.get('/', (req, res, next) => {
+//     res.send('ルートだよ！');
+// });
+
+// app.get('/hoge/fuga', (req, res, next) => {
+//     res.send('ほげふが');
+// });
+
+// app.get('/get/', (req, res, next) => {
+//     res.json({ message: `This is a pen.` });
+// });
+
+// app.get(/a/, function (req, res) {
+//     res.send('/a/')
+// })
+
+app.get('/ab*cd/', (req, res, next) => {
+    res.send(req.path)
+})
+
+app.get('/users/:userId/books/:bookId', function (req, res) {
+    res.send(req.params)
+})
 
 app.use((req, res) => {
     res.sendStatus(404);
